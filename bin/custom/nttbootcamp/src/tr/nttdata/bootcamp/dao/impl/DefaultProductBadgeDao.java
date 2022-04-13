854ebdc73fb71058285c653fe9f164e93923286f
Enes Tatlıcoğlu <enes.tatlicioglu@nttdata.com>
2022-04-13 18:20:32 +0300

    GenericSearch

--- bin/custom/nttbootcamp/src/tr/nttdata/bootcamp/dao/impl/DefaultProductBadgeDao.java
+++ bin/custom/nttbootcamp/src/tr/nttdata/bootcamp/dao/impl/DefaultProductBadgeDao.java
@@ -1,17 +1,26 @@
 package tr.nttdata.bootcamp.dao.impl;
 
+import de.hybris.platform.core.*;
 import de.hybris.platform.core.model.ItemModel;
+import de.hybris.platform.genericsearch.GenericSearchService;
 import de.hybris.platform.servicelayer.internal.dao.DefaultGenericDao;
 import de.hybris.platform.servicelayer.internal.dao.SortParameters;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import tr.nttdata.bootcamp.dao.ProductBadgeDao;
 import tr.nttdata.bootcamp.enums.BadgeStatus;
 import tr.nttdata.bootcamp.model.ProductBadgeModel;
 
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class DefaultProductBadgeDao extends DefaultGenericDao<ProductBadgeModel> implements ProductBadgeDao {
+
+    @Autowired
+    private GenericSearchService genericSearchService;
 
     public DefaultProductBadgeDao() {
         super(ProductBadgeModel._TYPECODE);
@@ -25,8 +34,21 @@ public class DefaultProductBadgeDao extends DefaultGenericDao<ProductBadgeModel>
 
     @Override
     public List<ProductBadgeModel> findActiveBadges() {
-        return find(Collections.singletonMap(ProductBadgeModel.STATUS, BadgeStatus.ACTIVE),
-                SortParameters.singletonDescending(ItemModel.MODIFIEDTIME));
+        final GenericQuery query = new GenericQuery(ProductBadgeModel._TYPECODE);
+        GenericSearchField statusField = new GenericSearchField(ProductBadgeModel.STATUS);
+        GenericSearchField startDateField = new GenericSearchField(ProductBadgeModel.STARTDATE);
+        GenericSearchField endDateField = new GenericSearchField(ProductBadgeModel.ENDDATE);
+        GenericSearchField modifiedTimeField = new GenericSearchField(ItemModel.MODIFIEDTIME);
+
+        final Date now = DateUtils.truncate(new Date(), Calendar.MINUTE); // For caching
+        query.addCondition(GenericCondition.createConditionForValueComparison(statusField, Operator.EQUAL, BadgeStatus.ACTIVE));
+        query.addCondition(GenericCondition.createConditionList(Operator.OR, GenericCondition.createIsNullCondition(startDateField),
+                GenericCondition.createConditionForValueComparison(startDateField, Operator.LESS_OR_EQUAL, now)));
+        query.addCondition(GenericCondition.createConditionList(Operator.OR, GenericCondition.createIsNullCondition(endDateField),
+                GenericCondition.createConditionForValueComparison(endDateField, Operator.GREATER, now)));
+        query.addOrderBy(new GenericSearchOrderBy(modifiedTimeField, false));
+
+        return genericSearchService.<ProductBadgeModel>search(query).getResult();
     }
 
 }
