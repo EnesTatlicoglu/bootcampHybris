3a9347d3482840ad09d3094cb5d3497059575e90
Enes Tatlıcoğlu <enes.tatlicoglu@gmail.com>
2022-04-25 16:01:25 +0300

    Transaction

--- bin/custom/nttbootcamp/src/tr/nttdata/bootcamp/service/impl/DefaultProductBadgeService.java
+++ bin/custom/nttbootcamp/src/tr/nttdata/bootcamp/service/impl/DefaultProductBadgeService.java
@@ -1,6 +1,8 @@
 package tr.nttdata.bootcamp.service.impl;
 
 import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.tx.Transaction;
+import de.hybris.platform.util.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,12 +34,29 @@ public class DefaultProductBadgeService implements ProductBadgeService {
             return badge;
         }
 
-        badge = modelService.create(ProductBadgeModel.class);
-        badge.setCode(code);
-        badge.setStatus(BadgeStatus.ACTIVE);
-        modelService.save(badge);
-        LOG.info("Created badge for code {}", code);
-        return badge;
+        Transaction tx = Transaction.current();
+        boolean success = false;
+        try {
+            tx.begin();
+            badge = modelService.create(ProductBadgeModel.class);
+            badge.setStatus(BadgeStatus.ACTIVE);
+            badge.setCode(code);
+            modelService.save(badge);
+            LOG.info("Created badge for code {}", code);
+            success = Config.getBoolean("create.badge.transaction.success", true);
+            return badge;
+        } catch (Exception ex){
+            LOG.error("An exception occurred while trying to create badge for code {}", code, ex);
+            return null;
+        } finally {
+            if(!success || tx.isRollbackOnly()){
+                LOG.info("Rolling back for badge creation of {}", code);
+                tx.rollback();
+            } else {
+                LOG.info("Committing for badge creation of {}", code);
+                tx.commit();
+            }
+        }
     }
 
     @Override
