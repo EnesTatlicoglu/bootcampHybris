11604bd0c9c8ecc10521a8f4f64d393134fbb302
Enes Tatlıcoğlu <enes.tatlicoglu@gmail.com>
2022-04-27 16:46:01 +0300

    Abortable Job

--- bin/custom/nttbootcamp/src/tr/nttdata/bootcamp/job/CheckActiveBadgesJob.java
+++ bin/custom/nttbootcamp/src/tr/nttdata/bootcamp/job/CheckActiveBadgesJob.java
@@ -2,6 +2,7 @@ package tr.nttdata.bootcamp.job;
 
 import de.hybris.platform.cronjob.enums.CronJobResult;
 import de.hybris.platform.cronjob.enums.CronJobStatus;
+import de.hybris.platform.cronjob.jalo.CronJobProgressTracker;
 import de.hybris.platform.cronjob.model.CronJobModel;
 import de.hybris.platform.servicelayer.cronjob.AbstractJobPerformable;
 import de.hybris.platform.servicelayer.cronjob.PerformResult;
@@ -12,6 +13,7 @@ import tr.nttdata.bootcamp.dao.ProductBadgeDao;
 import tr.nttdata.bootcamp.model.ProductBadgeModel;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CheckActiveBadgesJob extends AbstractJobPerformable<CronJobModel> {
 
@@ -21,9 +23,37 @@ public class CheckActiveBadgesJob extends AbstractJobPerformable<CronJobModel> {
 
     @Override
     public PerformResult perform(CronJobModel cronJob) {
+        final CronJobProgressTracker tracker = new CronJobProgressTracker(modelService.getSource(cronJob));
         final List<ProductBadgeModel> activeBadges = getProductBadgeDao().findActiveBadges();
         LOG.info("Found {} active badges!", CollectionUtils.size(activeBadges));
+        tracker.setProgress(1.0d); // Set initial process as %1
+        for (int i = 2; i < 100; i++)
+        {
+            if (clearAbortRequestedIfNeeded(cronJob))
+            {
+                LOG.error("The job is aborted at step {}", i);
+                return new PerformResult(CronJobResult.ERROR, CronJobStatus.ABORTED);
+            }
+            try
+            {
+                tracker.setProgress((double) i); // Set progress
+                int waitTime = ThreadLocalRandom.current().nextInt(100, 1000);
+                LOG.info("Will wait {} ms at step {}", waitTime, i);
+                Thread.sleep(waitTime);
+            }
+            catch (final InterruptedException e)
+            {
+                return new PerformResult(CronJobResult.FAILURE, CronJobStatus.ABORTED);
+            }
+        }
+        tracker.setProgress(100.0d); // Set progress as %100
+        tracker.close(); // Save last progress to the database
         return new PerformResult(CronJobResult.SUCCESS, CronJobStatus.FINISHED);
+    }
+
+    @Override
+    public boolean isAbortable() {
+        return true;
     }
 
     public ProductBadgeDao getProductBadgeDao() {
